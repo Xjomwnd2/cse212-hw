fcb59df8c998aeff478d6b5aa44489bf0ac3a2c7
YourGitHubUsername <jmwaura@byupathway.edu>
2025-05-22 02:25:55 +0300

    COMMIT

--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -1,33 +1,27 @@
-﻿public static class DisplaySums {
-    public static void Run() {
-        DisplaySumPairs([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-        // Should show something like (order does not matter):
-        // 6 4
-        // 7 3
-        // 8 2
-        // 9 1 
+﻿using System;
+using System.Collections.Generic;
 
-        Console.WriteLine("------------");
-        DisplaySumPairs([-20, -15, -10, -5, 0, 5, 10, 15, 20]);
-        // Should show something like (order does not matter):
-        // 10 0
-        // 15 -5
-        // 20 -10
-
-        Console.WriteLine("------------");
-        DisplaySumPairs([5, 11, 2, -4, 6, 8, -1]);
-        // Should show something like (order does not matter):
-        // 8 2
-        // -1 11
-    }
-
-    /// <summary>
-    /// Display pairs of numbers (no duplicates should be displayed) that sum to
-    /// 10 using a set in O(n) time.  We are assuming that there are no duplicates
-    /// in the list.
-    /// </summary>
-    /// <param name="numbers">array of integers</param>
-    private static void DisplaySumPairs(int[] numbers) {
-        // TODO Problem 2 - This should print pairs of numbers in the given array
+public class DisplaySums
+{
+    public static void DisplaySumPairs(List<int> numbers)
+    {
+        // Create a set to store numbers we've seen so far
+        HashSet<int> seenNumbers = new HashSet<int>();
+        
+        // Loop through each number in the list
+        foreach (var number in numbers)
+        {
+            // Calculate the complement of the current number
+            int complement = 10 - number;
+            
+            // If the complement is already in the set, we found a pair
+            if (seenNumbers.Contains(complement))
+            {
+                Console.WriteLine($"Pair found: ({complement}, {number})");
+            }
+            
+            // Add the current number to the set
+            seenNumbers.Add(number);
+        }
     }
 }
